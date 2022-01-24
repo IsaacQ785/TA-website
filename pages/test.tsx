4a65150ae6bb2b59c0b6e9cb9f63e97fde61ba0d
IsaacQ785 <70641752+IsaacQ785@users.noreply.github.com>
2022-01-24 13:54:34 +0000

    Adding test page file for new UI

---
+++ pages/test.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+export default function Home({ tickers }) {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
